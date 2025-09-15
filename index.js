433643d376573387b2fa6740f971fe15ad5e1b3f
Sri karthikeya <karthikyarra358@gmail.com>
2025-09-15 15:51:30 +0530

    index.js

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-const http = require("http");
-const fs = require("fs");
-const port = 4000;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const server = http.createServer(async (req, res) => {
-  try {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`<!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Document</title>
-        </head>
-        <body>
-          <h1>Hello World!!</h1>
-        </body>
-      </html>`);
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
-});
-
-server.listen(port, () => {
-  console.log(`server is listening at port ${port}`);
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
